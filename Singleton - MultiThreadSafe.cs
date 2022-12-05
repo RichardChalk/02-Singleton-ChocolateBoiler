76aaaf954f9a257d84a650adc4a1873dd2cf3ade
Richard Chalk <barmybrit@gmail.com>
2022-12-05 10:28:30 +0100

    Removed code

--- Singleton - MultiThreadSafe.cs
+++ Singleton - MultiThreadSafe.cs
@@ -6,22 +6,6 @@ namespace Chocolate_Lazy
 {
     class SingletonEager
     {
-        private static SingletonEager _singletonEager = new SingletonEager();
-        private int _counter;
-        private SingletonEager()
-        {
-            _counter = 0;
-        }
-
-        public static SingletonEager GetInstance()
-        {
-            // Eager instantiation
-            return _singletonEager;
-        }
-
-        public int GetNextId()
-        {
-            return ++_counter;
-        }
+        // Lycka till
     }
 }
--- Singleton.cs
+++ Singleton.cs
@@ -6,27 +6,6 @@ namespace Chocolate_Lazy
 {
     class Singleton
     {
-        private static Singleton _singleton;
-        private int _counter;
-        private Singleton()
-        {
-            _counter = 0;
-        }
-
-        public static Singleton GetInstance()
-        {
-            // Lazy instantiation
-            if (_singleton==null)
-            {
-                _singleton = new Singleton();
-            }
-
-            return _singleton;
-        }
-
-        public int GetNextId()
-        {
-            return ++_counter;
-        }
+        // Lycka till
     }
 }
